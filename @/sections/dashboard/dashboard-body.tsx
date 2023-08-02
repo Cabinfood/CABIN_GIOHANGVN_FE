cecc7faf1eb8d16afa3789e49e80e2fc8467c135
devkypham <132863359+devkypham@users.noreply.github.com>
2023-08-02 17:15:58 +0700

    feat: add redux, implement ui for each tab (#1)

--- @/sections/dashboard/dashboard-body.tsx
+++ @/sections/dashboard/dashboard-body.tsx
@@ -1,30 +1,71 @@
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import ProductSidebar from "./products/products-sidebar"
-import ListProduct from "./products/products-list"
+import ProductContent from "./products/products-content"
+import { useSelector } from "react-redux"
+import { selectTabState } from "redux/reducers/tab-slice"
+import SettingContent from "./settings/setting-content"
+import { TABS } from "@/mocks/common"
+import SettingSidebar from "./settings/setting-sidebar"
+import DesignContent from "./design/design-content"
+
+type TabRenderers = Record<string, () => JSX.Element>;
 
 type Props = {
   children?: ReactNode
 }
 function DashboardBody({ children }: Props) {
+  const tabState = useSelector(selectTabState);
+
+  const renderProductsTab = () => (
+    <>
+      <ProductSidebar />
+      <ProductContent />
+    </>
+  );
+
+  const renderDesignTab = () => (
+    <>
+      <ProductSidebar />
+      <DesignContent />
+    </>
+  );
+
+  const renderSettingsTab = () => (
+    <>
+      <SettingSidebar />
+      <SettingContent />
+    </>
+  );
+
+  const tabRenderers: TabRenderers = {
+    'products': renderProductsTab,
+    'design': renderDesignTab,
+    'settings': renderSettingsTab,
+  };
+  const currentTabRenderer = tabRenderers[tabState];
+
   return (
     <div className=" bg-slate-100">
       <div className="container mx-auto min-h-screen px-4 sm:px-6 lg:px-8">
-        <div className="flex h-14 w-full items-end justify-end">
-          <Select>
-            <SelectTrigger className="w-[180px] bg-white">
-              <SelectValue placeholder="Sort by name" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="light">Light</SelectItem>
-              <SelectItem value="dark">Dark</SelectItem>
-              <SelectItem value="system">System</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
+        {
+          tabState === TABS[0].value && <div className="flex h-14 w-full items-end justify-end">
+            <Select>
+              <SelectTrigger className="w-[180px] bg-white">
+                <SelectValue placeholder="Sort by name" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        }
         <div className="flex flex-nowrap gap-6 py-4">
-          <ProductSidebar />
-          <ListProduct />
+          {
+            currentTabRenderer && currentTabRenderer()
+          }
         </div>
       </div>
     </div>
---
+++ @/sections/dashboard/settings/setting-sidebar.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Cross2Icon, GridIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Switch } from "@/components/ui/switch"
+function SettingSidebar() {
+  return (
+    <Card className="w-80">
+      <CardContent className=":pt-0 px-2 py-2">
+        <div className="flex items-center justify-between p-3 rounded-md bg-slate-100">
+          <Label htmlFor="airplane-mode">General</Label>
+        </div>
+      </CardContent>
+      <CardContent className=":pt-0 px-2 py-2">
+        <div className="flex items-center justify-between p-3 rounded-md hover:bg-slate-100">
+          <Label htmlFor="airplane-mode">Billing</Label>
+        </div>
+      </CardContent>
+      <CardContent className=":pt-0 px-2 py-2">
+        <div className="flex items-center justify-between p-3 rounded-md hover:bg-slate-100">
+          <Label htmlFor="airplane-mode">Members</Label>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default SettingSidebar
